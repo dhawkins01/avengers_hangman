a17cee84d4ddd084194a6521d93dd09feab45e32
Dave Hawkins <dhawkins01@gmail.com>
2019-12-22 20:27:09 -0500

    added javascript that selects random word, and displays each letter represented as underscores on the screen

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,30 @@
-alert("connected");
+// words array of possible choices
+var words = ["iron man", "spider man", "hulk", "captain america", "thanos", "thor", "loki", "clint barton", "hawkeye", "black widow",
+"bucky barnes", "nick fury", "black panther", "vision", "starlord", "doctor strange", "falcon", "groot", "ant man", "rocket", "wasp",
+"tony stark", "peter parker", "steve rogers", "winter soldier"];
+
+var lettersGuessed = [];
+
+// starting variables
+var wins = 0;
+var guessesRemaining = 20;
+
+var randomWord = words[Math.floor(Math.random() * words.length)];
+var winningWord = document.querySelector("#winningWord");
+
+
+// display word on the screen represented by underscores
+displayWord();
+// main game loop
+// function declaration
+
+function displayWord(){
+    
+    for(var i = 0; i < randomWord.length; i++){
+        if(randomWord[i] === " ") {
+            winningWord.innerHTML += "&nbsp";
+        } else {
+            winningWord.textContent += "_ "
+        }
+    }
+}
