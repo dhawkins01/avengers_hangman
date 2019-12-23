1ecc91f5f4ff6cc205946d68b4f8d31f0f8610d6
Dave Hawkins <dhawkins01@gmail.com>
2019-12-22 21:22:24 -0500

    added onkeyup listener to check for letters pressed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,7 +3,7 @@ var words = ["iron man", "spider man", "hulk", "captain america", "thanos", "tho
 "bucky barnes", "nick fury", "black panther", "vision", "starlord", "doctor strange", "falcon", "groot", "ant man", "rocket", "wasp",
 "tony stark", "peter parker", "steve rogers", "winter soldier"];
 
-var lettersGuessed = [];
+var lettersGuessed = ["a", "b","t"];
 
 // starting variables
 var wins = 0;
@@ -11,11 +11,22 @@ var guessesRemaining = 20;
 
 var randomWord = words[Math.floor(Math.random() * words.length)];
 var winningWord = document.querySelector("#winningWord");
+var lettersRemaining = document.querySelector("#lettersRemaining");
+var winCount = document.querySelector("#wins");
+var guessedLetters = document.querySelector("#lettersGuessed");
+
+lettersRemaining.textContent = guessesRemaining;
+winCount.textContent = wins;
+guessedLetters.textContent = lettersGuessed;
+
+
 
 
 // display word on the screen represented by underscores
 displayWord();
-// main game loop
+
+
+
 // function declaration
 
 function displayWord(){
@@ -27,4 +38,13 @@ function displayWord(){
             winningWord.textContent += "_ "
         }
     }
+}
+
+document.onkeyup = function(event){
+    var userGuess = event.key;
+    console.log(userGuess);
+    guessesRemaining--;
+    lettersRemaining.textContent = guessesRemaining;
+
+
 }
